87c80cb3497426c09595e627d606d5fc28e63669
plinio <p.grijalbar@uniandes.edu.co>
2022-04-29 00:56:45 -0500

    Feature: Create test, should has 4 number of rows table

--- src/app/plant/plant-list/plant-list.component.spec.ts
+++ src/app/plant/plant-list/plant-list.component.spec.ts
@@ -1,25 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-import { PlantListComponent } from './plant-list.component';
+import faker from '@faker-js/faker';
+import {By} from '@angular/platform-browser';
+
+import {PlantListComponent} from './plant-list.component';
+import {DebugElement} from "@angular/core";
+import {Plant} from "../plant";
+
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
   let fixture: ComponentFixture<PlantListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PlantListComponent ]
+      declarations: [PlantListComponent],
+      imports: [HttpClientTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantListComponent);
     component = fixture.componentInstance;
+
+    const plants: Array<Plant> = new Array<Plant>();
+
+    for (let i = 0; i < 3; i++) {
+      plants.push(new Plant(
+        i + 1,
+        faker.name.findName(),
+        faker.name.findName(),
+        faker.datatype.string(),
+        faker.datatype.number(),
+        faker.lorem.words(),
+        faker.lorem.text()
+      ))
+    }
+
+    component.plants = plants;
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should has 4 number of rows table', () => {
+    fixture.detectChanges();
+    const rowDebugElements = debug.queryAll(By.css('table tr'));
+    expect(rowDebugElements.length).toBe(4);
   });
 });
